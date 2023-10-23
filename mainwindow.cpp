8a8f52ca926ebcb091ea3da242f0af90a98d9d7c
Aubin Septier <as875607@ad.u-bourgogne.fr>
2023-10-23 16:03:54 +0200

    Initial commit

---
+++ mainwindow.cpp
@@ -0,0 +1,35 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QDebug>
+
+#include "MfErrNo.h"
+#include "Sw_Device.h"
+#include "Sw_ISO14443A-3.h"
+#include "Sw_Mf_Classic.h"
+#include "Tools.h"
+#include "TypeDefs.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QWidget(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+ReaderName MyReader;
+
+void MainWindow::on_Connect_clicked(){
+    int16_t status = MI_OK;
+    MyReader.Type = ReaderCDC;
+    MyReader.device = 0;
+
+    status = OpenCOM(&MyReader);
+    qDebug() << "OpenCOM" << status ;
+
+}
+
